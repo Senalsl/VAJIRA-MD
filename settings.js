61ebf9c92e9ddd001417e7395a41d4645a4b20dd
Senalsl <Senalgihansa1@gmail.com>
2024-05-31 19:47:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ global.mess = {
 global.owner = [process.env.OWNER_NUMBER] || ['94784666782'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=bV8GxByT#6Edhz02P81_uSyTuVMBUzfgNtcmJpUWA9Xq3bhg7uhI',
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=eQEizaRC#lDhJ5Jo_dDACs7h-6dmGHg1I14NCLTSCx1dMypYsq4Y',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 LANG:  process.env.LANG || 'SI' ,    
@@ -35,10 +35,10 @@ AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
 AUTO_RECORDING:  process.env.AUTO_RECORDING  || false  ,
 AUTO_READ:  process.env.AUTO_READ  || false  ,
 AUTO_BIO:  process.env.AUTO_BIO  || false  ,    
-ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || true  ,            
+ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,            
 WORK_TYPE: process.env.WORK_TYPE || 'public'  ,
 ANTI_LINK: process.env.ANTI_LINK || false  ,
-ANTI_BOT: process.env.ANTI_BOT || tur  ,
+ANTI_BOT: process.env.ANTI_BOT || ture ,
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94784666782'  ,    
 ALIVE: process.env.ALIVE || `default`,
 OWNER_NAME: process.env.OWNER_NAME || 'Senesh_CM⃤' ,     
